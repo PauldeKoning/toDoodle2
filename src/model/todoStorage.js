884c4f05bfbf0337389fa62c1c0f7f592aa582c3
Pieter_W <163425907+Pieterwattel@users.noreply.github.com>
2025-04-29 18:00:06 +0200

    add some pseudocode specific to todoStorage
    this is pseudocode to order the todo's in order of urgency

--- src/model/todoStorage.js
+++ src/model/todoStorage.js
@@ -1,8 +1,38 @@
 const storage = {
-    array: [{
-        id: 2,
-        title: "yes"
-    }],
+    array: [],
+
+    arrayOrderedByUrgency: [],
+
+    orderByUrgency: function () {
+        
+    },
+
 }
 
 export { storage };
+
+/*
+* when a todo is added, either 
+    IF given date, use it
+        dateSpecifiedByUser = true
+
+    OR create a new date depending on urgency level
+        urgent - 3 days after today
+        soon - 10 days after today
+        casual - 30 days after today
+        dateSpecifiedByUser = false
+
+    add it to the storage.array
+
+* when todo is removed
+just remove that todo, no worries, they all have their date added
+
+- todo order is changed
+    IF dateSpecifiedByUser === TRUE AND nextTodo date === NOT the same date
+        IF NOT (confirm: if you change the place of this todo, it will lose its date
+            return
+
+    give the todo the date of the todo it was placed above.
+    dateSpecifiedByUser === FALSE
+    search the normal array, and place the todo in front of the todo it was placed above
+*/
