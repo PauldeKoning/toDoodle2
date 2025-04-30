98a47427605acd39b64ebb8e45693157ffe36080
Pieter_W <163425907+Pieterwattel@users.noreply.github.com>
2025-04-30 12:59:47 +0200

    add more information to the todo creation

--- src/model/todoItem.js
+++ src/model/todoItem.js
@@ -6,18 +6,14 @@ export default class TodoItem {
     constructor(todoDataObj) {
         this.id = todoDataObj.id;
         this.title = todoDataObj.title;
+        this.importance = todoDataObj.importance
+
+        this.finished = todoDataObj.finished
     }
 
     static id = 0
 
-    static create(todoDataObj) {
-        todoDataObj.id = this.id++
-        const newTodo = new TodoItem(todoDataObj)
-        return newTodo
-    }
-
     static saveTodo(todo) {
-        console.log(storage.array)
        storage.array.push(todo)
     }
 }
