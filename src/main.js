ac690822885b68d69f9486df686bd2f3f65e6cc1
Pieter_W <163425907+Pieterwattel@users.noreply.github.com>
2025-05-01 16:27:23 +0200

    update input for testing main.js

--- src/main.js
+++ src/main.js
@@ -1,39 +1,46 @@
-import "./styles.css";
-import todoController from "./controller/todoController";
-import { storage } from "./model/todoStorage";
+import './styles.css';
+import todoController from './controller/todoController';
+import { storage } from './model/todoStorage';
+import { currentDate } from './model/dates';
 
-console.log(storage.array)
+let testingIIFE = (function () {
+  console.log('hello');
 
-const todoTestArray = [
+  const todoTestArray = [
     {
-        title: "mostUrgentTodo",
-        id: 1,
-        importance: 1,
-        
+      title: 'mostUrgentTodo',
+      id: 1,
+      importance: 1,
+      lastDayOfDeadline: '2025 - 06 - 16',
+      dateSpecifiedByUser: true,
     },
     {
-        title: "casualTodo",
-        id: 3,
-        importance: 2,
+      title: 'casualTodo',
+      id: 3,
+      importance: 2,
+      lastDayOfDeadline: '2024 - 10 - 09',
+      dateSpecifiedByUser: false,
     },
     {
-        title: "normalTodo",
-        id: 4,
-        importance: 3,
+      title: 'normalTodo',
+      id: 4,
+      importance: 3,
+      lastDayOfDeadline: '2025 - 12 - 01',
+      dateSpecifiedByUser: true,
     },
     {
-        title: "regularTodo",
-        id: 5,
-        urgency: 2,
-        importance: 1,
+      title: 'regularTodo',
+      id: 5,
+      urgency: 2,
+      importance: 1,
+      lastDayOfDeadline: '2025 - 11 - 20',
+      dateSpecifiedByUser: false,
     },
-]
+  ];
 
-todoTestArray.forEach((element) => {
-    console.log(element)
-})
+  todoTestArray.forEach((element) => {
+    todoController.createTodo(element);
+  });
 
-
-
-todoController.removeTodo(obj, storage)
-console.log(storage.array)
+  console.log(storage.array);
+})();
