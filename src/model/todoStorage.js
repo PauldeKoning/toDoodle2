a8b64ded6cfabceffc9220768edfee1d5b3e8dfd
Paul de Koning <pauldekoning01@gmail.com>
2025-05-10 16:37:15 +0100

    Fix id bug

--- src/model/todoStorage.js
+++ src/model/todoStorage.js
@@ -5,6 +5,7 @@ export default class TodoStorage {
   constructor() {}
 
   static instance;
+  static currentId = 0;
 
   // Get a singleton class (singleton means there is only 1 instance of class!)
   // This is static as static means you can call the method without an instance being present
@@ -16,9 +17,14 @@ export default class TodoStorage {
     return this.instance;
   }
 
+  static GetNextId() {
+    TodoStorage.currentId++;
+    return TodoStorage.currentId;
+  }
+
   // Add an ID to the value then add
   addTodo(todoItem) {
-    todoItem.id = this.todos.length + 1;
+    todoItem.id = TodoStorage.GetNextId();
     this.todos.push(todoItem);
     return todoItem;
   }
